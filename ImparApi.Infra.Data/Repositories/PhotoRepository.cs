6f8e7858ad7eece9724379cf98f4cb562a5a500d
Anthony Silva <Anthony Silva>
2022-10-27 11:40:14 -0300

    adicionando services e repositories.

---
+++ ImparApi.Infra.Data/Repositories/PhotoRepository.cs
@@ -0,0 +1,13 @@
+﻿using ImparApi.Business.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImparApi.Infra.Data.Repositories
+{
+    public class PhotoRepository : IPhotoRepository
+    {
+    }
+}
