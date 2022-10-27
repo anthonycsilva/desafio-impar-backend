23cf0801aa8ac3dc6ea3e380f740b57cfd73404b
Anthony Silva <Anthony Silva>
2022-10-27 11:34:26 -0300

    Criando biblioteca de classes, pastas e principais entidades do desafio.

---
+++ ImparApi.Business/Entities/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImparApi.Business.Entities
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public int PhotoId { get; set; }
+        public Photo Photo { get; set; }
+        public string Name { get; set; }
+        public string Status { get; set; }
+
+    }
+}
