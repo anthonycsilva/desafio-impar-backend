6b96283c77e37d3c1ce174453be4e438e3f3e2b3
Anthony Silva <Anthony Silva>
2022-11-01 11:55:26 -0300

    implementando remover carro no repository.

--- ImparApi.Infra.Data/Repositories/CarRepository.cs
+++ ImparApi.Infra.Data/Repositories/CarRepository.cs
@@ -32,5 +32,15 @@ namespace ImparApi.Infra.Data.Repositories
                 .ToListAsync();
             return cars;
         }
+
+        public async Task RemoveCar(int carId)
+        {
+            var car = await _dataContext.Cars.Where(x => x.Id == carId).SingleAsync();
+            if (car is null)
+                throw new Exception("Carro nao foi encontrado para ser deletado");
+            _dataContext.Cars.Remove(car);
+            await _dataContext.SaveChangesAsync();
+
+        }
     }
 }
