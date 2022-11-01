27c2e38d91c3be30fdb774bee1023fb525438a5e
Anthony Silva <Anthony Silva>
2022-11-01 12:04:18 -0300

    configurando para excluir a foto associada ao carro.

--- ImparApi.Infra.Data/Repositories/CarRepository.cs
+++ ImparApi.Infra.Data/Repositories/CarRepository.cs
@@ -35,10 +35,14 @@ namespace ImparApi.Infra.Data.Repositories
 
         public async Task RemoveCar(int carId)
         {
-            var car = await _dataContext.Cars.Where(x => x.Id == carId).SingleAsync();
+            var car = await _dataContext.Cars
+            .Where(x => x.Id == carId)
+            .Include(x => x.Photo)
+            .SingleAsync();
             if (car is null)
                 throw new Exception("Carro nao foi encontrado para ser deletado");
             _dataContext.Cars.Remove(car);
+            _dataContext.Photos.Remove(car.Photo);
             await _dataContext.SaveChangesAsync();
 
         }
